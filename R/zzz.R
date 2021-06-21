99c51e85230a30252a659970c0b14bd86ba6ecdc
Lori Shepherd <lori.shepherd@roswellpark.org>
2021-06-21 12:55:44 -0400

    deprecate
     --unresponsive

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
 .onLoad <- function(libname, pkgname) {
   options(url = "http://mousefm.genehopper.de/rest/finemap/")
 }
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.15")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
